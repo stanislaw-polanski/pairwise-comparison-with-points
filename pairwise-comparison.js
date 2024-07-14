3acc997770ca802d47ea8b867100b9933fbda8b2
Stanisław Polański <stanislaw.polanski@hotmail.com>
2024-07-14 16:33:11 +0200

    added second grade points and draw possibility

--- pairwise-comparison.js
+++ pairwise-comparison.js
@@ -1,8 +1,9 @@
 class Item {
 
-  constructor(value, score) {
+  constructor(value, score, secondGradeScore) {
     this.value = value;
     this.score = score || 0;
+    this.secondGradeScore = secondGradeScore || 0;
   }
 }
 
@@ -14,23 +15,45 @@ class Pair {
     this.winner = null;
   }
 
+  WinningPoints = () => 3;
+  DrawPoints = () =>  1;
+  DrawLabel = () =>  "draw";
+
   get voted() {
     return this.winner != null;
   }
 
-  _voteFor(item) {
+  _voteFor(item, secondGradeScore) {
     if (!this.voted) {
-      item.score++;
+      item.score = item.score + this.WinningPoints();
+      item.secondGradeScore = item.secondGradeScore + secondGradeScore;
       this.winner = item;
+    }
+    console.log(item)
+  }
+
+  voteForDraw() {
+    if (!this.voted) {
+      this.item1.score = this.item1.score + this.DrawPoints();
+      this.item2.score = this.item2.score + this.DrawPoints();
+      this.winner = this.DrawLabel();
     }
   }
 
   voteForItem1() {
-    this._voteFor(this.item1);
+    this._voteFor(this.item1, this.WinningPoints());
   }
 
   voteForItem2() {
-    this._voteFor(this.item2);
+    this._voteFor(this.item2, this.WinningPoints());
+  }
+
+  voteForItem1WithPoints(points) {
+    this._voteFor(this.item1, points);
+  }
+
+  voteForItem2WithPoints(points) {
+    this._voteFor(this.item2, points);
   }
 
   isItem1Winner() {
